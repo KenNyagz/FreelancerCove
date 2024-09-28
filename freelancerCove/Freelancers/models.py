52780e963446e535076d7f01c19829b348122bdb
KenNyaga <kennyagz681@gmail.com>
2024-09-28 03:59:53 +0300

    Dependencies txt

--- freelancerCove/Freelancers/models.py
+++ freelancerCove/Freelancers/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+def freelancer(models.Model):
+    """Template for freelancer object"""
+    firstName = models.CharField(max_length=255)
+    lastName = models.CharField(max_length=255)
+    phone_no = models.IntegerField()
+    joined = models.DateField(default=timezone.now)
+    
